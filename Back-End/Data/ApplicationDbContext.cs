31c46a13f909f9f1bdb6ac7238b52d482a88389b
Chaitali Desale <chaitali.desale92@gmail.com>
2024-01-03 15:29:02 +0000

    Seeds data to the database

--- Back-End/Data/ApplicationDbContext.cs
+++ Back-End/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 using Back_End.Models;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 public class ApplicationDbContext : DbContext
 {
@@ -8,4 +9,20 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<Restaurant> Restaurants { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Restaurant>().HasData(SeedRestaurantData());
+    }
+
+    public List<Restaurant> SeedRestaurantData()
+    {
+        var restaurants = new List<Restaurant>();
+        using (StreamReader r = new StreamReader(@"restaurantData.json"))
+        {
+            string json = r.ReadToEnd();
+            restaurants = JsonConvert.DeserializeObject<List<Restaurant>>(json);
+        }
+        return restaurants;
+    }
 }
