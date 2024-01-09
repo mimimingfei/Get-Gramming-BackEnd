8154a6235ad872e6c268cedc7c6119147a295ef7
Chaitali Desale <chaitali.desale92@gmail.com>
2024-01-09 10:20:55 +0000

    Added userId in FeUser for usercontext

--- Back-End/Models/FeUser.cs
+++ Back-End/Models/FeUser.cs
@@ -3,13 +3,17 @@
     public class FeUser
     {
 
-        public FeUser(string userName, string email, string token) 
+        public FeUser(int userId, string userName, string email, string token) 
         {
+            UserId = userId;
             UserName = userName;
             Email = email;
             Token = token;
+
         }
 
+
+        public int UserId { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
